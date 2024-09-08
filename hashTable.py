ea15885f7b9970df91e15a433a3e347ef4f77809
Prafulla Pradhan <ppradhan22638@ucumberlands.edu>
2024-09-08 11:32:04 -0500

    Deliverable1: Added hash table

---
+++ hashTable.py
@@ -0,0 +1,9 @@
+class hashTable:
+    def __init__(self):
+        self.table = {}
+
+    def insert(self, key, value):
+        self.table[key] = value
+
+    def get(self, key):
+        return self.table.get(key)
